6abdc6fb64cb7ef174149421e3e46572b1eb7c77
Nader Nabil <nadernabilmansour@gmail.com>
2019-03-12 09:07:23 +0200

    Editing some definations to be consistent macros defination , adding Read-Write-Toggle Pin

--- Drivers/DIO Read-Write.c
+++ Drivers/DIO Read-Write.c
@@ -1,29 +1,33 @@
+#include "tm4c123gh6pm.h"
+#include "Utils.h"
+#include "Std_Types.h"
+#include "DIO.h"
 
 void DIO_vid_WritePortVal(u8 port , u8 val)
 {
 	switch(port)
 		{
-			case PORTA:
+			case portA:
 				GPIO_PORTA_DATA_R = val;
 				break;
 
-			case PORTB:
+			case portB:
 				GPIO_PORTB_DATA_R = val;
 				break;
 
-			case PORTC:
+			case portC:
 				GPIO_PORTC_DATA_R = val;
 				break;
 
-			case PORTD:
+			case portD:
 				GPIO_PORTD_DATA_R = val;
 				break;
 			
-			case PORTE:
+			case portE:
 				GPIO_PORTE_DATA_R = val;
 				break;
 			
-			case PORTF:
+			case portF:
 				GPIO_PORTF_DATA_R = val;
 				break;
 		
@@ -38,27 +42,27 @@ u8 DIO_u8ReadPortVal (u8 port)
 
 	switch(port)
 		{
-			case PORTA:
+			case portA:
 				ReadVal = GPIO_PORTA_DATA_R;
 				break;
 
-			case PORTB:
+			case portB:
 				ReadVal = GPIO_PORTB_DATA_R;
 				break;
 
-			case PORTC:
+			case portC:
 				ReadVal = GPIO_PORTC_DATA_R;
 				break;
 
-			case PORTD:
+			case portD:
 				ReadVal = GPIO_PORTD_DATA_R;
 				break;
 			
-			case PORTE:
+			case portE:
 				ReadVal = GPIO_PORTE_DATA_R;
 				break;
 			
-			case PORTF:
+			case portF:
 				ReadVal = GPIO_PORTF_DATA_R;
 				break;
 
@@ -70,29 +74,133 @@ void DIO_vid_TogglePortVal(u8 port){
 	
 	switch(port)
 		{
-			case PORTA:
+			case portA:
 				GPIO_PORTA_DATA_R = GPIO_PORTA_DATA_R ^ mask;
 				break;
 
-			case PORTB:
+			case portB:
 				GPIO_PORTB_DATA_R = GPIO_PORTB_DATA_R ^ mask;
 				break;
 
-			case PORTC:
+			case portC:
 				GPIO_PORTC_DATA_R = GPIO_PORTC_DATA_R ^ mask;
 				break;
 
-			case PORTD:
+			case portD:
 				GPIO_PORTD_DATA_R = GPIO_PORTD_DATA_R ^ mask;
 				break;
 			
-			case PORTE:
+			case portE:
 				GPIO_PORTE_DATA_R = GPIO_PORTE_DATA_R ^ mask;
 				break;
 			
-			case PORTF:
+			case portF:
 				GPIO_PORTF_DATA_R = GPIO_PORTF_DATA_R ^ mask;
 				break;
 		}
 	
 }
+
+void DIO_vid_WritePinVal(u8 port , u8 pin , u8 val)
+{
+	
+	switch(port)
+		{
+			case portA :
+				assign_bit(GPIO_PORTA_DATA_R,pin,val);
+				break;
+				
+
+			case portB :
+				assign_bit(GPIO_PORTB_DATA_R,pin,val);
+				break;
+
+			case portC :
+				assign_bit(GPIO_PORTC_DATA_R,pin,val);
+				break;
+
+			case portD :
+				assign_bit(GPIO_PORTD_DATA_R,pin,val);
+				break;
+					
+			case portE :
+				assign_bit(GPIO_PORTE_DATA_R,pin,val);
+				break;
+			
+			case portF :
+				assign_bit(GPIO_PORTF_DATA_R,pin,val);
+				break;
+
+			default:
+				break;
+		}
+}
+
+
+u8 DIO_u8ReadPinVal (u8 port , u8 pin ){
+	u8 ReadVal = 2;
+
+	switch(port)
+		{
+			case portA:
+				ReadVal = get_bit(GPIO_PORTA_DATA_R,pin);
+				break;
+
+			case portB:
+				ReadVal = get_bit(GPIO_PORTB_DATA_R,pin);
+				break;
+
+			case portC:
+				ReadVal = get_bit(GPIO_PORTC_DATA_R,pin);
+				break;
+
+			case portD:
+				ReadVal = get_bit(GPIO_PORTD_DATA_R,pin);
+				break;
+			
+			case portE:
+				ReadVal = get_bit(GPIO_PORTE_DATA_R,pin);
+				break;
+			
+			case portE:
+				ReadVal = get_bit(GPIO_PORTF_DATA_R,pin);
+				break;
+
+		}
+	
+		return ReadVal;
+}
+
+void DIO_vid_TogglePinVal(u8 port , u8 pin){
+	
+	switch(port)
+		{
+			case portA:
+				toggle_bit(GPIO_PORTA_DATA_R,pin);
+				break;
+
+			case portB:
+				toggle_bit(GPIO_PORTB_DATA_R,pin);
+				break;
+
+			case portC:
+				toggle_bit(GPIO_PORTC_DATA_R,pin);
+				break;
+
+			case portD:
+				toggle_bit(GPIO_PORTD_DATA_R,pin);
+				break;
+			
+			case portE:
+				toggle_bit(GPIO_PORTE_DATA_R,pin);
+				break;
+			
+			case portF:
+				toggle_bit(GPIO_PORTF_DATA_R,pin);
+				break;
+		}
+	
+}
+
+
+
