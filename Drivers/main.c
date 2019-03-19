633368c953f7a45e651ef597029417ff7a99ef3e
amirrami <36231711+amirrami@users.noreply.github.com>
2019-03-19 23:53:57 +0200

    finishing main function

---
+++ Drivers/main.c
@@ -0,0 +1,49 @@
+#include "tm4c123gh6pm.h"
+#include "std_types.h"
+#include "utlisis.h"
+#include "DIO.h"
+#include "7seg.h"
+#include "LCD.h"
+
+
+int main(void)
+{ 
+	u8 in;
+	s32 counter=0;
+	LCD_vidInit();
+	LCD_vidDisplayInt(1);
+	while(1)
+	{
+			//in = SW1_Input();
+		if(!DIO_ReadPin(portF,4))
+			{ 
+				counter++;
+				LCD_vidWriteCommand(1);
+				if(counter>999) counter=0;
+				LCD_vidDisplayInt(counter);
+				delay_ms (200);
+	}
+		if(!DIO_ReadPin(portF,3))
+			{ while(!DIO_ReadPin(portF,3));
+				counter--;
+				if(counter<0) counter=0;
+				LCD_vidWriteCommand(1);
+				delay_ms(50);
+				LCD_vidDisplayInt(counter);
+				delay_ms (50);
+	}		
+	
+	
+			if(!DIO_ReadPin(portF,2))
+			{ while(!DIO_ReadPin(portF,2));
+				counter=0;
+				LCD_vidWriteCommand(1);
+				delay_ms(50);
+				LCD_vidDisplayInt(counter);
+				delay_ms (50);
+	}	
+	
+	}
+
+}
+
