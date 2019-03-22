708f739a1b2cd2cef2c1f1038c0b544c31505e3b
Nader Nabil <36284564+Nadern96@users.noreply.github.com>
2019-03-22 02:13:43 +0200

    Create bushbuttons.h

---
+++ Drivers/bushbuttons.h
@@ -0,0 +1,4 @@
+ void init_bushbutton ( u8 port , u8 pin ) ;
+ u32 is_pressed_increment ( u8 port , u8 pin); 
+ u32 is_pressed_decrement  (u8 port , u8 pin) ;
+ void is_pressed_reset(u8 port , u8 pin) ;
