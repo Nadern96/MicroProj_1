db30076021ebdc71be54c0f516776a0029362fca
Nader Nabil <36284564+Nadern96@users.noreply.github.com>
2019-03-22 02:04:44 +0200

    avoid using definations inside .c files

--- Drivers/pullup_down .c
+++ Drivers/pullup_down .c
@@ -7,44 +7,44 @@
  
 void Port_vid_SetPinPullUp (u8 port , u8 pin){
 	switch (port){
-		case PORTA:
+		case 0:
 			set_bit(GPIO_PORTA_PUR_R,pin);
 			break;
-		case PORTB:
+		case 1:
 			set_bit(GPIO_PORTB_PUR_R,pin);
 			break;
-		case PORTC:
+		case 2:
 			set_bit(GPIO_PORTC_PUR_R,pin);
 			break;
-		case PORTD:
+		case 3:
 			set_bit(GPIO_PORTD_PUR_R,pin);
 			break;
-		case PORTE:
+		case 4:
 			set_bit(GPIO_PORTE_PUR_R,pin);
 			break;
-		case PORTF:
+		case 5:
 			set_bit(GPIO_PORTF_PUR_R,pin);
 			break;
 	}
 }
 void Port_vid_SetPinPullDown (u8 port , u8 pin){
 	switch (port){
-		case PORTA:
+		case 0:
 			set_bit(GPIO_PORTA_PDR_R,pin);
 			break;
-		case PORTB:
+		case 1:
 			set_bit(GPIO_PORTB_PDR_R,pin);
 			break;
-		case PORTC:
+		case 2:
 			set_bit(GPIO_PORTC_PDR_R,pin);
 			break;
-		case PORTD:
+		case 3:
 			set_bit(GPIO_PORTD_PDR_R,pin);
 			break;
-		case PORTE:
+		case 4:
 			set_bit(GPIO_PORTE_PDR_R,pin);
 			break;
-		case PORTF:
+		case 5:
 			set_bit(GPIO_PORTF_PDR_R,pin);
 			break;
 	}
