e87473fb5fe29139b11ac9c555aeefe50b466603
Nader Nabil <36284564+Nadern96@users.noreply.github.com>
2019-03-22 02:25:57 +0200

    Update LCD.c

--- Drivers/LCD.c
+++ Drivers/LCD.c
@@ -9,64 +9,48 @@
 #include "utlisis.h"
 #include "DIO.h"
 #include "LCD.h"
+#include "delay.h"
 
- u32 counter ;
- 
- 
- void delay_ms (u32 time ) {
- u32 count ;
- while (time >0 )
- {
-	count = 16000;			// to delay time ms for 80Mhz 
-	while (count >0)
-	{
-	count =count-1;
-	
-	} 
-	time =time-1;
- 
- }
-}
 void LCD_vidWriteCommand (u8 command)
 {
 	//RW = 0 to write
-	DIO_WritePin(LCD_Control,LCD_RW,LOW);
+	DIO_vid_WritePinVal(LCD_Control,LCD_RW,LOW);
 
 	//RS = 0 for command
-	DIO_WritePin(LCD_Control,LCD_RS,LOW);
+	DIO_vid_WritePinVal(LCD_Control,LCD_RS,LOW);
 
 	//PORT = command
 	DIO_vid_WritePortVal(LCD_Data,command);
 
 	// E = 1 enable =1 to start exec
 
-	DIO_WritePin(LCD_Control,LCD_E,HIGH);
+	DIO_vid_WritePinVal(LCD_Control,LCD_E,HIGH);
 
 	//leave E= 1 (opened) for 2milliseconds until data is displayed
 	delay_ms(2);
 
-	DIO_WritePin(LCD_Control,LCD_E,LOW);
+	DIO_vid_WritePinVal(LCD_Control,LCD_E,LOW);
 }
 
 void LCD_vidWriteData (u8 data)
 {
 	//RW = 0 to write
-	DIO_WritePin(LCD_Control,LCD_RW,LOW);
+	DIO_vid_WritePinVal(LCD_Control,LCD_RW,LOW);
 
 	//RS = 0 for command
-	DIO_WritePin(LCD_Control,LCD_RS,HIGH);
+	DIO_vid_WritePinVal(LCD_Control,LCD_RS,HIGH);
 
 	//PORT = data
 	DIO_vid_WritePortVal(LCD_Data,data);
 
 	// E = 1 enable =1 to start exec
 
-	DIO_WritePin(LCD_Control,LCD_E,HIGH);
+	DIO_vid_WritePinVal(LCD_Control,LCD_E,HIGH);
 
 	//leave E= 1 (opened) for 2milliseconds until data is displayed
 	delay_ms(2);
 
-	DIO_WritePin(LCD_Control,LCD_E,LOW);
+	DIO_vid_WritePinVal(LCD_Control,LCD_E,LOW);
 
 }
 
