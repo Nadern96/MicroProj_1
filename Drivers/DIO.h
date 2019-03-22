40afa8db0a98c55510ea42fc342ecb468265b13c
Nader Nabil <36284564+Nadern96@users.noreply.github.com>
2019-03-22 02:20:59 +0200

    Update DIO.h

--- Drivers/DIO.h
+++ Drivers/DIO.h
@@ -18,10 +18,16 @@
 
 void Port_Init(u8 port_index);
 void Port_SetPinDirection(u8 PORT, u8 PIN, u8 DIR);
-u8 DIO_ReadPort(u8 PORT, u8 PIN);
-void DIO_WritePort(u8 PORT, u8 PIN, u8 VALUE);
-void DIO_PinPullUp(u8 PORT, u8 PIN);
-void DIO_PinPullDown(u8 PORT, u8 PIN);
+void Port_vid_SetPortDir(u8 port, u8 Dir);
+void Port_vid_SetPinDir(u8 port , u8 pin , u8 Dir);
+u8 DIO_u8ReadPinVal (u8 port , u8 pin );
+u8 DIO_u8ReadPortVal (u8 port);
+void DIO_vid_WritePinVal(u8 port , u8 pin , u8 val);
+void DIO_vid_WritePortVal(u8 port , u8 val);
+void DIO_vid_TogglePinVal(u8 port , u8 pin){;
+void DIO_vid_TogglePortVal(u8 port);
+void Port_vid_SetPinPullUp (u8 port , u8 pin);                                            
+void Port_vid_SetPinPullDown (u8 port , u8 pin)
 
 #endif /* DIO_H_ */
 
